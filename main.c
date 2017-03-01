d08ee9114704af2346d6e8fe2af05ce53ff37ff7
Ashraf Reda Mohamed <eng.ashrafreda@gmail.com>
2017-03-01 12:39:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 int main(){
 		
 	Struct_USART USART_Init_Struct;
-	USART_Init_Struct.BuadRate			=	Rate_9600;
+	USART_Init_Struct.BuadRate		=	Rate_9600;
 	USART_Init_Struct.Operation_Mode	=	Asynchronous_Normal_Speed;
 	USART_Init_Struct.Duplex_Select		=	TX_Mode;
 	USART_Init_Struct.Character_Size	=	Char_8_Bits;
 	USART_Init_Struct.Parity_Mode		=	No_Parity;
-	USART_Init_Struct.Stop_Bit			=	One_Bit;
+	USART_Init_Struct.Stop_Bit		=	One_Bit;
 	USART_Init(USART_Init_Struct);
 	
 	USART_Transmit_String("Hello World!");
